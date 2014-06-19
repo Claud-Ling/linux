460da7af2965dc7bc2c25a9fc56256c61a9087c2
tonyhe <Tony_He@sigmadesigns.com>
2014-06-19 19:20:14 +0800

    [Sigma bug# 38001] Position explicit delay after set dbg_swen to assue safe access to debug regs
    Change-Id: Ib6c5762d686f31aa5a9131fe571cffe5cf7a2c61

--- arch/arm/kernel/hw_breakpoint.c
+++ arch/arm/kernel/hw_breakpoint.c
@@ -1035,16 +1035,18 @@ static struct notifier_block __cpuinitdata dbg_reset_nb = {
 
 #ifdef CONFIG_ARCH_SIGMA_SX6
 #include <plat/io.h>
+#include <linux/delay.h>
 /*
  * it should be called before CPU accesses any debug registers
  */
 static void sx6_swdbg_enable(void)
 {
-#define SX6_A9_CFG2_REG ((void*)0x1500ef12)	//[7..6] sw debug enable (to access dbg regs), each bit for one core
+#define SX6_A9_CFG2_REG ((void*)0x1500ef12)	/*[7..6] sw debug enable (to access dbg regs), each bit for one core*/
 #define DBG_SWEN_MASK	0xc0
 #define DBG_SWEN_SHIFT	6
 	MWriteRegByte(SX6_A9_CFG2_REG, 3 << DBG_SWEN_SHIFT, DBG_SWEN_MASK);	/*enable sw debug on two cores*/
 	mb();	/*drain write buffer*/
+	ndelay(200)	/*explicit delay (~200ns) is required here as dbg_swen is in 24M clock domain*/
 }
 #endif
 
