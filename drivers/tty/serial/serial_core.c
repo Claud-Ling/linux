fcd8e82e282f3ea7ad41418bf479c7abacd24683
Lucas Stach <l.stach@pengutronix.de>
2017-05-11 12:56:14 +0200

    serial: core: fix crash in uart_suspend_port
    [ Upstream commit 88e2582e90bb89fe895ff0dceeb5d5ab65d07997 ]
    
    With serdev we might end up with serial ports that have no cdev exported
    to userspace, as they are used as the bus interface to other devices. In
    that case serial_match_port() won't be able to find a matching tty_dev.
    
    Skip the irq wakeup enabling in that case, as serdev will make sure to
    keep the port active, as long as there are devices depending on it.
    
    Change-Id: I57b4946001cdea57e98c2ecd2bc89708a39256e8
    Fixes: 8ee3fde04758 (tty_port: register tty ports with serdev bus)
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>

--- drivers/tty/serial/serial_core.c
+++ drivers/tty/serial/serial_core.c
@@ -2026,13 +2026,7 @@ int uart_suspend_port(struct uart_driver *drv, struct uart_port *uport)
 	mutex_lock(&port->mutex);
 
 	tty_dev = device_find_child(uport->dev, &match, serial_match_port);
-#if defined (CONFIG_PROC_FS) && defined (CONFIG_SIGMA_DTV)
-	if(tty_dev == NULL) {
-		mutex_unlock(&port->mutex);
-		return 0;
-	}
-#endif
-	if (device_may_wakeup(tty_dev)) {
+	if (tty_dev && device_may_wakeup(tty_dev)) {
 		if (!enable_irq_wake(uport->irq))
 			uport->irq_wake = 1;
 		put_device(tty_dev);
